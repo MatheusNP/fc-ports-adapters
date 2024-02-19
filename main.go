27cf3c8fc8f14f46cbbc5862755e50e8a825517c
Matheus Nobre <matheusnobre56@gmail.com>
2024-02-18 21:10:46 -0300

    feat: add cobra cli

--- main.go
+++ main.go
@@ -1,19 +1,11 @@
+/*
+Copyright © 2024 NAME HERE <EMAIL ADDRESS>
+
+*/
 package main
 
-import (
-	"database/sql"
-
-	"github.com/MatheusNP/fc-ports-adapters/adapters/db"
-	"github.com/MatheusNP/fc-ports-adapters/application"
-	_ "github.com/mattn/go-sqlite3"
-)
+import "github.com/MatheusNP/fc-ports-adapters/cmd"
 
 func main() {
-	sqlDB, _ := sql.Open("sqlite3", "sqlite.db")
-
-	productDBAdapter := db.NewProductDB(sqlDB)
-
-	productService := application.NewProductService(productDBAdapter)
-
-	productService.Create("Product Example", 30)
+	cmd.Execute()
 }
